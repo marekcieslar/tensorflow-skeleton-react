352096044e0af0910b542f26decffb644b80b955
Marek Cieślar <marek.cieslar@gmail.com>
2023-12-15 13:00:26 +0100

    added video

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
-function App() {
+import { FC, useEffect, useRef } from 'react';
+
+import style from './App.module.scss';
+
+const App: FC = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const constraints: MediaStreamConstraints = {
+      video: true,
+    };
+
+    let stream: MediaStream | null = null;
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then((str) => {
+        if (videoRef.current) {
+          stream = str;
+          videoRef.current.srcObject = str;
+        } else {
+          str.getTracks().forEach((track) => {
+            track.stop();
+          });
+        }
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.error('Error accessing the camera:', err);
+      });
+
+    return () => {
+      if (stream) {
+        const tracks = stream?.getTracks() || [];
+        tracks.forEach((track) => {
+          track.stop();
+        });
+      }
+    };
+  }, []);
+
   return (
-    <>
-      <h1>test</h1>
-    </>
+    <div>
+      <video className={style.video} ref={videoRef} autoPlay playsInline>
+        <track kind="captions" />
+      </video>
+    </div>
   );
-}
+};
 
 export default App;
